ca4e525a00329e84f54c9ffd9bd307eebf91afa6
Mateusz Pyzowski <managervcf@gmail.com>
2018-11-07 13:07:38 +0800

    Add remote database hosted by mLab

--- models/index.js
+++ models/index.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/todo-api', {useNewUrlParser: true});
+const databaseURL = process.env.DATABASEURL || 'mongodb://127.0.0.1:27017/todo-api'
 
+mongoose.connect(databaseURL, {useNewUrlParser: true});
 mongoose.set('debug', true);
-
 mongoose.Promise = Promise;
 
 const todoModel = require('./todo');
