5235f4bb1f94507f6fa96ac015ec1ed4a2ec6254
Mateusz Pyzowski <managervcf@gmail.com>
2018-11-07 12:51:23 +0800

    Add cors library to prevent cross-origin errors

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const todoRoutes = require('./routes/todos');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
-  res.sendFile('index.html'); 
-});
+app.get('/', (req, res) =>
+	res.sendFile('index.html')
+);
 
 app.use('/api/todos', todoRoutes);
 
-app.listen(port);
+app.listen(port, () =>
+	console.log(`Server is running on port ${port}`)
+);
