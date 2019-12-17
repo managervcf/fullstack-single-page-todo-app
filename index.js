dcf9d806e55169dbbc8c4f1983bf371d2eb4e4b9
Mateusz <managervcf@gmail.com>
2019-12-17 08:54:31 +0100

    Comment server file.

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+// Require modules
 const express = require('express');
-const bodyParser = require('body-parser');
-const todoRoutes = require('./routes/todos');
 const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
 
+// Require routes
+const todoRoutes = require('./routes/todos');
+
+// Configuration
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Use routes
 app.use('/api/todos', todoRoutes);
 
 if (process.env.NODE_ENV === 'production') {
@@ -13,10 +19,10 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 	// Express will serve up the index.html file
 	// if it doesn't recognize the route
-	const path = require('path');
 	app.get('*', (req, res) =>
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 	);
 }
 
+// Start the server
 app.listen(process.env.PORT || 6000);
